af933eb308586f5c4193773b34cdcc5fc802a1ec
Knut Åbjørsbråten <knutsindre@gmail.com>
2026-01-30 10:20:01 +0100

    Removed debug elements

--- src/feature/board/editor/diagram-board/BoardDiagram.ts
+++ src/feature/board/editor/diagram-board/BoardDiagram.ts
@@ -1,11 +1,6 @@
 import { Diagram } from "../diagram-core/Diagram";
 import { ElementChangeCallback } from "../diagram-core/types";
 import { BoardIdleMode, OnTaskCreateCallback } from "./modes";
-import { TaskElement } from "./elements/TaskElement";
-
-const TEST_TASK_COUNT = 50;
-const TEST_AREA_WIDTH = 3000;
-const TEST_AREA_HEIGHT = 2000;
 
 export interface BoardDiagramOptions {
   onTaskCreate?: OnTaskCreateCallback;
@@ -21,18 +16,5 @@ export function createBoardDiagram(
     onElementChange: options?.onElementChange,
   });
   diagram.start();
-  addTestTaskElements(diagram);
   return diagram;
-}
-
-function addTestTaskElements(diagram: Diagram): void {
-  const stageManager = diagram.getStageManager();
-  const layer = stageManager.addLayer("tasks");
-
-  for (let i = 0; i < TEST_TASK_COUNT; i++) {
-    const task = new TaskElement(`Task #${i + 1}`);
-    task.posX = Math.random() * TEST_AREA_WIDTH;
-    task.posY = Math.random() * TEST_AREA_HEIGHT;
-    stageManager.addElement(layer.id, task);
-  }
 }
